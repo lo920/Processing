232ef58aac449066f73938462e6f67e50397ee62
lo920 <70361295+lo920@users.noreply.github.com>
2020-09-04 16:40:41 +0900

    initial commit

---
+++ sketch________/sketch________.js
@@ -0,0 +1,12 @@
+function setup() {point(250, 250);
+  createCanvas(500, 500);
+  point(250, 250);
+  line(0, 0, 500, 500);
+  ellipse(250, 250, 300, 300)
+ triangle(250, 100, 100, 250, 400, 250);
+quad(250, 100, 100, 250, 400, 250, 250, 400);
+}
+
+function draw() {
+
+}
